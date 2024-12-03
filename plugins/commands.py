ec7d59ea83262c40f4393ca1d7e434bcb78789e8
Suhail1626 <104824273+Suhail1626@users.noreply.github.com>
2024-12-03 18:11:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Movie𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/wudixh')
+                InlineKeyboardButton('🤖 Movie𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/CinemaColonyLinks')
             ],
             [
                 InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', url=f"https://t.me/{temp.U_NAME}?start=help"),
